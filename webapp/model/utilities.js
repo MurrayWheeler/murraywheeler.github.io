7e1fdeb838096093402935d79dbc516e41002e6c
Murray Wheeler <murraywheeler@gmail.com>
2025-09-09 11:44:56 +1200

    remove full screen

--- webapp/model/utilities.js
+++ webapp/model/utilities.js
@@ -9,15 +9,15 @@ function isEmpty(obj) {
 
 // Function to request full screen mode
 async function requestFullScreen() {
-    try {
-        // Going to fullscreen. Need to be linked to a button press (I don't know why)
-        // https://wiki.appstudio.dev/How_to_run_fullscreen_in_an_Android_Chrome_app
-        document.documentElement.webkitRequestFullScreen();
-        console.log('Full screen mode');
-        resetTimeout();
-    } catch (err) {
-        console.error('Failed to go full screen:', err);
-    }
+    // try {
+    //     // Going to fullscreen. Need to be linked to a button press (I don't know why)
+    //     // https://wiki.appstudio.dev/How_to_run_fullscreen_in_an_Android_Chrome_app
+    //     document.documentElement.webkitRequestFullScreen();
+    //     console.log('Full screen mode');
+    //     resetTimeout();
+    // } catch (err) {
+    //     console.error('Failed to go full screen:', err);
+    // }
 }
 
 
--- webapp/view/pageMainView.js
+++ webapp/view/pageMainView.js
@@ -60,7 +60,7 @@ fiveCrowns.pageMainView = (function () {
       }
       hBox.addItem(playerCount);
       hBox.addItem(new sap.m.ToolbarSpacer({ width: "30px" }));
-      playButton = new sap.m.Button({ text: "Play >" });
+      playButton = new sap.m.Button({ text: "Play now >" });
       playButton.attachPress(function () { fiveCrowns.pageMainController.onPlayButton(oApp) });
       hBox.addItem(playButton);
 
