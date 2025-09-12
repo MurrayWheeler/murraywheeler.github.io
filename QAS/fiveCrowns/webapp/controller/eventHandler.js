ae5b9e01e9e16d332c9ae51bbfd93435c08567ce
Murray Wheeler <murraywheeler@gmail.com>
2025-09-12 15:56:08 +1200

    Swipe handling

--- QAS/fiveCrowns/webapp/controller/eventHandler.js
+++ QAS/fiveCrowns/webapp/controller/eventHandler.js
@@ -2,8 +2,10 @@
 
 
 fiveCrowns.eventHandler = (function() {
-    var touchstartY = 0;
-    var touchendY = 0;
+    var touchStartX = 0,
+        touchstartY = 0,
+        touchendX = 0,
+        touchendY = 0;
     var swipeThreshold = 50; // Minimum distance for a swipe
 
     return {
@@ -17,7 +19,8 @@ fiveCrowns.eventHandler = (function() {
             document.addEventListener('touchstart', function(event) {
                 // Only track single-finger touches
                 if (event.touches.length === 1) {
-                    touchstartY = event.touches[0].clientY;
+                    touchStartX = event.touches[0].clientX;
+                    touchstartY = event.touches[0].clientY; // For pull-to-refresh prevention
                 }
             }, { passive: false });
 
@@ -37,12 +40,52 @@ fiveCrowns.eventHandler = (function() {
             // Optional: Listen for touchend to perform a custom action
             document.addEventListener('touchend', function(event) {
                 if (event.changedTouches.length === 1) {
+                    touchendX = event.changedTouches[0].clientX;
                     touchendY = event.changedTouches[0].clientY;
+
                     // Check if it was a downward swipe from the top
                     if (window.scrollY === 0 && touchendY > touchstartY + swipeThreshold) {
                         console.log("Swipe down detected!");
                         // You can trigger a custom action here, like a manual refresh.
                         // For example: sap.m.MessageToast.show("Refresh action triggered!");
+                    }
+
+                    var deltaX = touchendX - touchStartX;
+                    var deltaY = touchendY - touchstartY;
+                    var currentPageId = fiveCrowns.model.getApp().getCurrentPage().getId();
+
+                    // Check for swipe left on main page
+                    if (currentPageId === "pageMain") {
+                        // Check for a left swipe that is more horizontal than vertical
+                        if (deltaX < -swipeThreshold && Math.abs(deltaX) > Math.abs(deltaY)) {
+                            fiveCrowns.pageMainController.onResumeGame(oApp);
+                        }
+                    } else {
+                        // Check for swipe right on any other page to go back
+                        if (deltaX > swipeThreshold && Math.abs(deltaX) > Math.abs(deltaY)) {
+                            switch (currentPageId) {
+                                case "pageGame":
+                                case "pageGameLandscape":
+                                    fiveCrowns.pageGameController.onBack(oApp);
+                                    break;
+                                case "pageGames":
+                                    fiveCrowns.pageGamesController.onGamesBack(oApp);
+                                    break;
+                                case "pageReorder":
+                                    fiveCrowns.pageReorderController.onReorderBack(oApp);
+                                    break;
+                                case "pageChangeDealer":
+                                    fiveCrowns.pageChangeDealerController.onChangeDealerBack(oApp);
+                                    break;
+                                case "pageSettings":
+                                    fiveCrowns.pageSettingsController.onSettingsBack(oApp);
+                                    break;
+                                case "pageStatistics":
+                                case "pageInstr":
+                                    oApp.back(); // These pages have a simple back navigation
+                                    break;
+                            }
+                        }
                     }
                 }
             }, { passive: false });
